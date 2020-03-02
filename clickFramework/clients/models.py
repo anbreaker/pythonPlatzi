6a003f8c22618071e3c0742eafc642a740535c73
anbreaker <antunez19@gmail.com>
2020-03-02 21:28:12 +0100

    Ejecuntando por comando ok

--- clickFramework/clients/models.py
+++ clickFramework/clients/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+
 class Client:
     def __init__(self, name, company, email, position, uid=None):
         self.name = name
@@ -7,14 +8,12 @@ class Client:
         self.email = email
         self.position = position
         self.uid = uid or uuid.uuid4()
-        
+
     def to_dict(self):
         # Para convertir a dict y escribir en csv
         return vars(self)
-    
+
     @staticmethod
     # No recibe self porque no necesita una instacia
     def schema():
         return ['name', 'company', 'email', 'position', 'uid']
-    
-    
